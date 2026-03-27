f9a505c4ffb1c1cce1158bc307ff38568e194fac
Bhakti Deshpande <bhaktideshpande8010@gmail.com>
2026-03-27 13:22:34 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+a=78.45
+print(a)
+print(type(a))
+
+b="fortune"
+print(b)
+print(type(b))
+
+c=0
+print(c)
+print(type(c))
+
+d=True
+print(d)
+print(type(d))
---
+++ loops.py
@@ -0,0 +1,43 @@
+# For Loop 10 program
+
+# print 1 to 10 series
+for i in range(1,11) :
+    print(i , end=" ")
+print()
+
+# print 1 to 10 in 2 differance
+for j in range(1,11,2) :
+    print(j)
+print()
+
+
+# print 10 to 1 
+for c  in range(11,0,-1) :
+    print(c)
+print()
+
+
+# Print 2's Table
+for s in range(1,11):
+    print(s*2, end=" ")
+print()
+
+
+# print odd number
+for o in range(1,30) :
+    if o % 2 !=0 :
+        print(o)
+
+# pyramid
+
+
+
+
+# # while LOOP
+# k=2
+# while(k<=10):
+#     print(k,end=" ")
+#     k=k+1
+
+
+
