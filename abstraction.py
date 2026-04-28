ed664bc1e9a1e7bffb9bd6ecb449e91a1bdec7e4
Bhakti Deshpande <bhaktideshpande8010@gmail.com>
2026-04-28 23:28:23 +0530

    abstraction"

---
+++ abstraction.py
@@ -0,0 +1,17 @@
+from abc import ABC,abstractclassmethod
+
+class Payment(ABC):
+    def print_slip(self,amount):
+        print("Your payment was done with Rs.",amount)
+
+        @abstractclassmethod
+        def payment(self):
+            print("a")
+
+class credit(Payment):
+    def payment(self,amount):
+        print("Credit crd payment done with Rs.",amount)
+
+c=credit()
+c.payment(1000)
+c.print_slip(2000)
