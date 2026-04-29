1bda99d21c7e54467ae2198b5616d6acd21dc7a7
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-04-29 16:08:48 +0530

    Module

--- module2.py
+++ module2.py
@@ -1,1 +1,7 @@
+import test_module                
+from test_module import add
+
 print("welcom to add web")
+
+add()       #importning Add() function from test_module file and running this file 
+test_module.sub()
--- test_module.py
+++ test_module.py
@@ -1,2 +1,5 @@
 def add():
     print("This is Addition")
+
+def sub():
+    print("this is substraction")
