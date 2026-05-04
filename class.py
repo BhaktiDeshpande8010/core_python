176227732229950748b3ffeb2814485e7bd8a081
Bhakti Deshpande <deshpandeb1204@gmail.com>
2026-05-04 13:15:30 +0530

    class

---
+++ class.py
@@ -0,0 +1,11 @@
+class abc:                          #creating a class
+    id=102
+    name="Python class"
+
+a=abc()                             #object of the class
+print(a.id)
+print(a.name)
+
+b=abc()                             # we can create multiple object of the class
+print(b.id)
+print(b.name)
